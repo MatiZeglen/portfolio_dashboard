6147c6d4f39d4f7601dc56a8199a4533e27216a4
MatiZeglen <matizeglen2003@gmail.com>
2024-03-20 17:06:50 +0100

    WiP

--- script.js
+++ script.js
@@ -51,3 +51,15 @@ window.addEventListener('resize', () =>
         searchForm.classList.remove('show');
     }
 });
+const toggler = document.getElementById('theme-toggle');
+toggler.addEventListener('change', function()
+{
+    if(this.checked)
+    {
+        document.body.classList.add('dark');
+    }
+    else
+    {
+        document.body.classList.remove('dark');
+    }
+});
