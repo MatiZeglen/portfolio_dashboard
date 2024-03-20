f9dc0e7156323232a262a18071135c8a3695ac57
MatiZeglen <matizeglen2003@gmail.com>
2024-03-20 16:18:32 +0100

    Script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-asdf
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+sideLinks.forEach(item=>
+    {
+        const li=item.parentElement;
+        item.addEventListener('click', () =>
+        {
+            sideLinks.forEach(i =>
+                {
+                    i.parentElement.classList.remove('active');
+                })
+                li.classList.add('active');
+        })
+    });
+
