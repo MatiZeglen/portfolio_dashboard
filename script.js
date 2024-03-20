133a11d13ae859dd4dd9f83bafef483240b979f5
MatiZeglen <matizeglen2003@gmail.com>
2024-03-20 16:41:27 +0100

    WiP

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ menuBar.addEventListener('click', () =>
 {
      sideBar.classList.toggle('close');
 })
+
+const searchBtn = document.querySelector('.content nav form .form-input button');
+const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+const searchForm = document.querySelector('.content nav form');
+
+searchBtn.addEventListener('click', function (e)
+{
+    if(window.innerWidth < 576)
+    {
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if(searchForm.classList.contains('show'))
+        {
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        }else
+        {
+            searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+});
